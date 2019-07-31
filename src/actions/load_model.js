84bfdbbbbf81a477b7ad290d4a8ca724b36f1cdf
Kiran Adhikari <kiruu1238@gmail.com>
2019-07-31 18:52:48 +0545

    URL Changed

--- src/actions/load_model.js
+++ src/actions/load_model.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-import { BASE_URL } from './api';
+import { MODEL_URL } from './api';
 
 export const load_model = (img) => async dispatch => {
     try {
-        const res = await axios.post(`${BASE_URL}/api/model`, img);
+        const res = await axios.post(MODEL_URL, img);
         return res;
     } catch (e) {
         throw e
