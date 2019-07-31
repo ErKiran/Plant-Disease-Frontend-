eed582dd5e4b16f8aa7753a585e1fbd3f04bfd71
Kiran Adhikari <kiruu1238@gmail.com>
2019-08-01 00:04:05 +0545

    CORS Django

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import { Provider } from 'react-redux';
 import store from './store';
 
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+axios.defaults.xsrfCookieName = "csrftoken";
 
 ReactDOM.render(
     <Provider store={store}>
