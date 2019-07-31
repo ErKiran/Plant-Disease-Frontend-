6fdb0d427db0639ae31c8de21b6c8942e0892727
Kiran Adhikari <kiruu1238@gmail.com>
2019-07-31 19:21:32 +0545

    Trailing Slash API Error

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,4 +1,4 @@
 //export const BASE_URL = 'https://calm-eyrie-44469.herokuapp.com';
 //export const BASE_URL = 'http://localhost:5000';
 export const BASE_URL = 'https://secure-beach-52412.herokuapp.com'
-export const MODEL_URL = 'http://localhost:8000/api/predict'
+export const MODEL_URL = 'http://localhost:8000/api/predict/'
