0a90454f3396e23689a9ee7881bcb3bc6925685b
Kiran Adhikari <kiruu1238@gmail.com>
2019-08-04 12:37:10 +0545

    Stats Changed

--- src/components/Aboutus/Stats.js
+++ src/components/Aboutus/Stats.js
@@ -10,30 +10,30 @@ class Stats extends Component {
                             <div className="row">
                                 <div className="col-lg-3 col-6">
                                     <div className="counter">
-                                        <span className="fa fa-smile-o"></span>
-                                        <div className="timer count-title count-number mt-2">5100</div>
-                                        <p className="count-text text-uppercase">happy customers</p>
+                                        <span className="fa fa-pagelines"></span>
+                                        <div className="timer count-title count-number mt-2">43755</div>
+                                        <p className="count-text text-uppercase">Training Images</p>
                                     </div>
                                 </div>
                                 <div className="col-lg-3 col-6">
                                     <div className="counter">
                                         <span className="fa fa-pagelines"></span>
-                                        <div className="timer count-title count-number mt-2">2271</div>
-                                        <p className="count-text text-uppercase">Disease Detected</p>
+                                        <div className="timer count-title count-number mt-2">10550</div>
+                                        <p className="count-text text-uppercase">Validation Images</p>
                                     </div>
                                 </div>
                                 <div className="col-lg-3 col-6 mt-lg-0 mt-5">
                                     <div className="counter">
                                         <span className="fa fa-users"></span>
-                                        <div className="timer count-title count-number mt-2">1120+</div>
-                                        <p className="count-text text-uppercase">Farmers</p>
+                                        <div className="timer count-title count-number mt-2">38</div>
+                                        <p className="count-text text-uppercase">Image Classes</p>
                                     </div>
                                 </div>
                                 <div className="col-lg-3 col-6 mt-lg-0 mt-5">
                                     <div className="counter">
                                         <span className="fa fa-leaf"></span>
-                                        <div className="timer count-title count-number mt-2">2690</div>
-                                        <p className="count-text text-uppercase">Reviews</p>
+                                        <div className="timer count-title count-number mt-2">224Hr</div>
+                                        <p className="count-text text-uppercase">Hours Dedicated</p>
                                     </div>
                                 </div>
                             </div>
